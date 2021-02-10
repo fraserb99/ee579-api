dd7627c20d6028fc96be1235ff627ed0163a859a
rossbroom99 <rossbroomfield@btinternet.com>
2021-02-10 20:16:01 +0000

    basically nothing

--- EE579/EE579.Core/Slices/Tenants/ITenantService.cs
+++ EE579/EE579.Core/Slices/Tenants/ITenantService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EE579.Core.Infrastructure.Services;
 using EE579.Core.Slices.Tenants.Models;
+using EE579.Domain.Entities;
 
 namespace EE579.Core.Slices.Tenants
 {
-    public interface ITenantService
+    public interface ITenantService : ICrudAppService<Tenant, TenantDto>
     {
         public IEnumerable<TenantDto> Get();
     }
