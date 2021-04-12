6de9528dbed1dd886cbecbc19eb3fdefec5e9089
fraserb99 <fraser.bell11@googlemail.com>
2021-04-12 20:51:21 +0100

    completed device group functionality
    dk

---
+++ EE579/EE579.Core/Slices/Devices/Mapping/MACStringToDeviceConverter.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using EE579.Domain;
+using EE579.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EE579.Core.Slices.Devices.Mapping
+{
+    public class MACStringToDeviceConverter : ITypeConverter<string, Device>
+    {
+        private readonly DatabaseContext _context;
+        public MACStringToDeviceConverter(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public Device Convert(string source, Device destination, ResolutionContext context)
+        {
+            return _context.Devices.FirstOrDefault(x => x.Id == source);
+        }
+    }
+}
