08ae06534fb739be9ba1c0e3313a85172232225f
fraserb99 <fraser.bell11@googlemail.com>
2021-04-12 20:55:29 +0100

    removed redundant methods

--- EE579/EE579.Core/Slices/DeviceGroups/Impl/DeviceGroupService.cs
+++ EE579/EE579.Core/Slices/DeviceGroups/Impl/DeviceGroupService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using EE579.Core.Infrastructure.Services;
+using EE579.Core.Slices.DeviceGroups.Models;
+using EE579.Domain;
+using EE579.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,13 @@ using System.Threading.Tasks;
 
 namespace EE579.Core.Slices.DeviceGroups.Impl
 {
-    class DeviceGroupService
+    public class DeviceGroupService : CrudAppService<DeviceGroup, DeviceGroupInput>, IDeviceGroupService
     {
+        public DeviceGroupService(DatabaseContext context, IMapper mapper) : base(context, mapper)
+        {
+
+        }
+        
+
     }
 }
