b0d65e9254d645f11048533ee0004b3c9ab42fea
Fraser Bell <fraser.bell11@googlemail.com>
2021-04-13 16:54:31 +0100

    add environment variables

--- EE579/EE579.Api/Program.cs
+++ EE579/EE579.Api/Program.cs
@@ -29,6 +29,10 @@ namespace EE579.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
