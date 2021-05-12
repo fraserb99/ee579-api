854a25bdcd1013989930a6579e327b3e96efc2b2
Fraser Bell <fraser.bell11@googlemail.com>
2021-05-12 11:43:54 +0100

    fixing device messages

---
+++ EE579/EE579.Domain/Entities/DeviceMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EE579.Domain.Entities
+{
+    public class DeviceMessage : EntityWithGuid
+    {
+        public string DeviceId { get; set; }
+        public string MessageBody { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ EE579/EE579.Domain/Migrations/20210512103540_AddDeviceMessages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EE579.Domain.Migrations
+{
+    public partial class AddDeviceMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "RuleOutputs",
+                newName: "OutputType");
+
+            migrationBuilder.CreateTable(
+                name: "DeviceMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeviceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MessageBody = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceMessages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeviceMessages");
+
+            migrationBuilder.RenameColumn(
+                name: "OutputType",
+                table: "RuleOutputs",
+                newName: "Type");
+        }
+    }
+}
